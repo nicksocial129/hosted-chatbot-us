4b93b4f84d4a8779d2ffeed99e10312ea230e43e
ForgeFlow AI <nickcprince@gmail.com>
2025-02-22 00:18:35 -0600

    branding-final

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -12,10 +12,10 @@ import { ReactNode } from "react"
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
-const APP_NAME = "Chatbot UI"
-const APP_DEFAULT_TITLE = "Chatbot UI"
-const APP_TITLE_TEMPLATE = "%s - Chatbot UI"
-const APP_DESCRIPTION = "Chabot UI PWA!"
+const APP_NAME = "Forgeflow Private"
+const APP_DEFAULT_TITLE = "Forgeflow Private"
+const APP_TITLE_TEMPLATE = "%s - Forgeflow Private"
+const APP_DESCRIPTION = "Forgeflow Private AI Chat!"
 
 interface RootLayoutProps {
   children: ReactNode
